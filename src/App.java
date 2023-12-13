c6f861ed0358961f400e882b39b0067ba06ae94c
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-12-13 10:02:59 +0100

    Gjort klar jav lek 10 upg 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner t = new Scanner(System.in);
+
+        System.out.println("Ange namn: ");
+        String namn = t.nextLine();
+        System.out.println("Ange lösenord: ");
+        String lösenord = t.nextLine();
+
+        if(namn.equals("root") && lösenord.equals("pass")){
+            System.out.println("Ditt bankkonto har 0 kr");
+        }
+        else{
+            System.out.println("Fel namn och lösenord");
+        }
     }
 }
